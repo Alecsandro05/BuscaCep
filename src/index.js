7ffd1841f3e33ba707218a89eb68d6fae99a7d02
Alecsandro05 <alecsandrosales1@gmail.com>
2024-10-21 21:00:11 -0300

    created all functions requestAPi

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
 const selecElementOption = document.getElementById("select")
 const result = document.getElementById("result")
-function showCep({ cep, bairro, logradouro, complemento, cidade, uf }) {
+
+function showCep({ cep, bairro, logradouro, complemento, localidade, uf }) {
   document.getElementById("textCep").innerText = `CEP: ${cep}`
   document.getElementById("logradouro").innerText = `logradouro: ${logradouro}`
   document.getElementById(
     "complemento"
   ).innerText = `complemento: ${complemento}`
   document.getElementById("bairro").innerText = `bairro: ${bairro}`
-  document.getElementById("cidade").innerText = `cidade: ${cidade}`
+  document.getElementById("cidade").innerText = `cidade: ${localidade}`
   document.getElementById("UF").innerText = `UF: ${uf}`
-  console.log(cep)
 }
 function callApiAjax(cep) {
-  // criar o objeto de requisição
   if (cep.length === 8) {
     const url = `https://viacep.com.br/ws/${cep}/json/`
+    // criar o objeto de requisição
     const xhttp = new XMLHttpRequest()
+    const startTime = performance.now()
     xhttp.open("GET", url, true)
     xhttp.onload = () => {
       if (xhttp.status === 200) {
+        const endTime = performance.now()
+        const responseTime = endTime - startTime
+        document.getElementById(
+          "responseTime"
+        ).innerText = `Tempo de resposta: ${responseTime.toPrecision(
+          3
+        )} milissegundos`
         const response = JSON.parse(xhttp.responseText)
 
-        console.log(response)
         showCep(response)
         result.classList.remove("hidden")
         result.classList.add("flex")
+        result.setAttribute("id", "ajax")
       } else {
         console.error("Erro ao buscar CEP:", xhttp.statusText)
       }
@@ -32,6 +40,64 @@ function callApiAjax(cep) {
     xhttp.send()
   } else {
     alert("CEP inválido. Por favor, digite um CEP com 8 dígitos.")
+    selecElementOption.selectedIndex = 0
+  }
+}
+
+function callApiFetch(cep) {
+  if (cep.length === 8) {
+    const startTime = performance.now()
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          alert("CEP não encontrado!")
+        } else {
+          const endTime = performance.now()
+          const responseTime = endTime - startTime
+          document.getElementById(
+            "responseTime"
+          ).innerText = `Tempo de resposta: ${responseTime.toPrecision(
+            3
+          )} milissegundos`
+          showCep(data)
+          result.classList.remove("hidden")
+          result.classList.add("flex")
+          result.setAttribute("id", "fetchApi")
+        }
+      })
+      .catch((error) => {
+        alert("Erro na requisição")
+      })
+  } else {
+    alert("CEP inválido. Por favor, digite um CEP com 8 dígitos.")
+    selecElementOption.selectedIndex = 0
+  }
+}
+async function callApiAsyncAndAwait(_cep) {
+  const startTime = performance.now()
+  if (_cep.length === 8) {
+    try {
+      const api = await fetch(`https://viacep.com.br/ws/${_cep}/json/`)
+      const cep = await api.json()
+      const endTime = performance.now()
+      const responseTime = endTime - startTime
+      document.getElementById(
+        "responseTime"
+      ).innerText = `Tempo de resposta: ${responseTime.toPrecision(
+        3
+      )} milissegundos`
+      showCep(cep)
+      result.classList.remove("hidden")
+      result.classList.add("flex")
+      result.setAttribute("id", "async")
+      showCep(cep)
+    } catch (error) {
+      alert("Erro ao buscar CEP: " + error)
+    }
+  } else {
+    alert("CEP inválido. Por favor, digite um CEP com 8 dígitos.")
+    selecElementOption.selectedIndex = 0
   }
 }
 
@@ -45,7 +111,11 @@ selecElementOption.addEventListener("change", () => {
     case "ajax":
       callApiAjax(clearCep)
       break
-
+    case "fethcApi":
+      callApiFetch(clearCep)
+      break
+    case "async":
+      callApiAsyncAndAwait(clearCep)
     default:
       break
   }
